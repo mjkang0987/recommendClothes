d48da611eb143cc2071269f74b0528f1a63b6025
minjeongkang <snowdrop1202@gmail.com>
2020-12-29 23:10:10 +0900

    feat: footer 마크업 추가

---
+++ clothes/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = _ => {
+  return (
+    <footer>
+      <span className="year">2020</span>Made by MJ Kang
+    </footer>
+  );
+};
+
+export default Footer;
