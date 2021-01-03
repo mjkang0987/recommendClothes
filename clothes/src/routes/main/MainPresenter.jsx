b9cda67454c2c538ea9571ba434ed8f4add94746
minjeongkang <snowdrop1202@gmail.com>
2021-01-03 22:41:29 +0900

    style: 메인 컨텐츠 영역 className 직관적으로 변경

--- clothes/src/routes/main/MainPresenter.jsx
+++ clothes/src/routes/main/MainPresenter.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 
 const MainPresenter = _ => {
   return (
-    <section className="contentWrap">
+    <>
       <h2>
         <span>오늘 날씨</span>
         <button
           type="button"
-          className="buttonRefresh"
+          className="btn-refresh"
           onClick="location.reload()">
           새로고침
         </button>
       </h2>
       <article
-        className="temperature"
+        className="now-wrap"
         data-temp="freezing">
-        <strong className="nowTempWrap">
+        <strong className="now-temp">
           <span>-5</span>
           <em>℃</em>
         </strong>
-        <div className="weatherInfo">
-          <span className="weather">
-            <span className="timeType">밤,</span>
-            <span className="weatherType">튼구름</span>
+        <div className="now-weather-temp">
+          <span className="now-weather">
+            <span className="now-time">밤,</span>
+            <span className="now-weather">튼구름</span>
           </span>
-          <span className="todayTemperature">
-            <span className="maxWrap">
+          <span className="now-temp-pitch">
+            <span className="now-temp-max">
               <span className="number">-5</span>
               <em>℃</em>
             </span>
-            <span className="minWrap">
+            <span className="now-temp-min">
               <span className="number">-5</span>
               <em>℃</em>
             </span>
@@ -38,7 +38,7 @@ const MainPresenter = _ => {
       </article>
       <h2>오늘 뭐입지?</h2>
       <article className="clothes">
-        <div className="recommendClothes">
+        <div className="clothes-recommend">
           <strong>대충 겨울 옷</strong>
           <ul>
             <li>야상</li>
@@ -50,7 +50,7 @@ const MainPresenter = _ => {
           </ul>
         </div>
       </article>
-    </section>
+    </>
   );
 };
 
