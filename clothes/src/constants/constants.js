ee0633ccf25f5557ad251aaf59a2fef22e16ef13
minjeongkang <snowdrop1202@gmail.com>
2021-01-12 23:17:29 +0900

    feat: constants.js API 정보 추가

--- clothes/src/constants/constants.js
+++ clothes/src/constants/constants.js
@@ -1,8 +1,13 @@
+const API = {
+  URL: 'https://api.openweathermap.org/data/2.5/weather',
+  KEY: '&lang=kr&appid=8d0fba684264cc55f9b735566611ac78'
+};
 const INITIAL_DASH = {
   DOUBLE: '--',
   QUARTER: '----'
 };
 
 export {
+  API,
   INITIAL_DASH
 }
