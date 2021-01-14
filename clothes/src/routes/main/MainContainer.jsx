2ad7511f1802c13a0dd20c3c1061b7c9bab0c9f7
minjeongkang <snowdrop1202@gmail.com>
2021-01-14 23:37:07 +0900

    refactor: API 정보 공통으로 사용하기 위해 MainContainer.jsx에서 삭제

--- clothes/src/routes/main/MainContainer.jsx
+++ clothes/src/routes/main/MainContainer.jsx
@@ -1,24 +1,8 @@
 import MainPresenter from './MainPresenter';
-import { fetchData } from '../../action/fetch';
-import { API } from '../../constants/constants';
-import { createContext } from 'react';
-
-const {getData} = fetchData;
-const {URL, KEY} = API;
-
-const data = async _ => {
-  return await getData({url: `${URL}?q=seoul${KEY}`});
-};
-
-const test = data();
-const Weather = createContext('');
 
 const MainContainer = _ => {
-console.log(test);
   return (
-    <Weather.Provider value={data}>
-      <MainPresenter/>
-    </Weather.Provider>
+    <MainPresenter/>
   );
 };
 
