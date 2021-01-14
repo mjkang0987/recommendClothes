716e5b9ae365e641f4ba6572919751ab8dd4a8d2
minjeongkang <snowdrop1202@gmail.com>
2021-01-14 23:52:13 +0900

    feat: 현재 도시의 날씨 API 정보 context 생성

--- clothes/src/App.js
+++ clothes/src/App.js
@@ -1,13 +1,44 @@
 import Header from './components/Header';
 import Main from './routes/main/index';
 import Footer from './components/Footer';
+import { fetchData } from './action/fetch';
+import { API, INITIAL_WEATHER } from './constants/constants';
+import { createContext, useState } from 'react';
+import { useOnMounted } from './action/useOnMount';
 
+const {getData} = fetchData;
+const {URL, KEY} = API;
+const {CITY, TEMP, LOCATION, WEATHER} = INITIAL_WEATHER;
+const weatherObject = {TEMP, LOCATION, WEATHER};
+
+const Weather = createContext(weatherObject);
 function App() {
+  const [weather, setWeather] = useState(weatherObject);
+  const [city, setCity] = useState(CITY);
+
+  const getWeather = async _ => {
+    return await getData({url: `${URL}?q=${city}${KEY}`});
+  };
+
+  const data = _ => {
+    getWeather()
+      .then(res => {
+        setWeather(res);
+        console.log(res);
+      });
+  };
+
+  useOnMounted(_ => {
+    data();
+  });
+
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Weather.Provider value={weather}>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </Weather.Provider>
     </>
   );
 }
