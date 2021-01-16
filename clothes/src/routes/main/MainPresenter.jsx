f3f00c932882a4390b0b7e1eac6b7974b1185cfe
minjeongkang <snowdrop1202@gmail.com>
2021-01-16 23:41:17 +0900

    feat: MainPresenter.jsx 날씨 context DOM 내부 추가

--- clothes/src/routes/main/MainPresenter.jsx
+++ clothes/src/routes/main/MainPresenter.jsx
@@ -1,4 +1,12 @@
+import { useContext } from 'react';
+import { Weather } from '../../App';
+import { INITIAL_DASH} from '../../constants/constants';
+
 const MainPresenter = _ => {
+  const temps = useContext(Weather);
+  const {TEMPS, WEATHER} = temps;
+  const {NOW, MIN, MAX} = TEMPS;
+  const {DOUBLE, QUARTER} = INITIAL_DASH;
   return (
     <>
       <h2>
@@ -13,21 +21,21 @@ const MainPresenter = _ => {
         className="now-wrap"
         data-temp="freezing">
         <strong className="now-temp">
-          <span>-5</span>
+          <span>{NOW ? NOW : DOUBLE}</span>
           <em>℃</em>
         </strong>
         <div className="now-weather-temp">
           <span className="now-weather">
             <span className="now-time">밤,</span>
-            <span className="now-weather">튼구름</span>
+            <span className="now-weather">{WEATHER ? WEATHER : QUARTER}</span>
           </span>
           <span className="now-temp-pitch">
             <span className="now-temp-max">
-              <span className="number">-5</span>
+              <span className="number">{MIN ? MIN : DOUBLE}</span>
               <em>℃</em>
             </span>
             <span className="now-temp-min">
-              <span className="number">-5</span>
+              <span className="number">{MAX ? MAX : DOUBLE}</span>
               <em>℃</em>
             </span>
           </span>
@@ -36,7 +44,7 @@ const MainPresenter = _ => {
       <h2>오늘 뭐입지?</h2>
       <article className="clothes">
         <div className="clothes-recommend">
-          <strong>대충 겨울 옷</strong>
+          <strong>{WEATHER ? DOUBLE : DOUBLE}</strong>
           <ul>
             <li>야상</li>
             <li>패딩</li>
