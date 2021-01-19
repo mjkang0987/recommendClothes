fdb06fdf1222f928fdb376b619414555a293ec82
minjeongkang <snowdrop1202@gmail.com>
2021-01-19 23:27:22 +0900

    feat: header component API 장소, 현재 시간 노출

--- clothes/src/components/Header.jsx
+++ clothes/src/components/Header.jsx
@@ -16,11 +16,11 @@ const Header = _ => {
         <span>메뉴 보기</span>
       </button>
       <span className="header-info">
-        <strong className="header-location">{QUARTER}</strong>
+        <strong className="header-location">{LOCATION ? CITIES[LOCATION] : QUARTER}</strong>
         <span className="header-time">
           <span className="header-time-type">{QUARTER}</span>
-          <span className="header-time-hour">{DOUBLE}</span>
-          <span className="header-time-minute">{DOUBLE}</span>
+          <span className="header-time-hour">{hour && hour < 10 ? `0${hour}` : `${hour}`}</span>
+          <span className="header-time-minute">{minute && minute < 10 ? `0${minute}` : `${minute}`}</span>
         </span>
       </span>
       <button
