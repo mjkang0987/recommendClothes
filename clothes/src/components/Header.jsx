8d20afb1b52f4474d758d8531a7b0230de953ad6
minjeongkang <snowdrop1202@gmail.com>
2021-01-20 00:06:49 +0900

    feat: header component 시간 분기
    * 0일 경우 false 처리로 삼항연산자 비교 값 ''으로 변경
    * 오전/오후 텍스트 분기

--- clothes/src/components/Header.jsx
+++ clothes/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Weather } from '../App';
-import { INITIAL_DASH, CITIES } from '../constants/constants';
+import { INITIAL_DASH, CITIES, TIME_TEXT } from '../constants/constants';
 import { useContext } from 'react';
 
 const Header = _ => {
@@ -7,6 +7,7 @@ const Header = _ => {
   const {hour, minute} = value.time;
   const {LOCATION} = value.weather;
   const {DOUBLE, QUARTER} = INITIAL_DASH;
+  const {MORNING, AFTERNOON} = TIME_TEXT;
 
   return (
     <header className="header">
@@ -18,12 +19,14 @@ const Header = _ => {
       <span className="header-info">
         <strong className="header-location">{LOCATION ? CITIES[LOCATION] : QUARTER}</strong>
         <span className="header-time">
-          <span className="header-time-type">{QUARTER}</span>
+          <span className="header-time-type">
+            {hour !== '' ? (hour < 12 ? MORNING[0] : AFTERNOON[0]) : QUARTER}
+          </span>
           <span className="header-time-hour">
-            {hour ? (hour < 10 ? `0${hour}` : `${hour}`) : DOUBLE}
+            {hour !== '' ? (hour < 10 ? `0${hour}` : `${hour}`) : DOUBLE}
           </span>
           <span className="header-time-minute">
-            {minute ? (minute < 10 ? `0${minute}` : `${minute}`) : DOUBLE}</span>
+            {minute !== '' ? (minute < 10 ? `0${minute}` : `${minute}`) : DOUBLE}</span>
         </span>
       </span>
       <button
