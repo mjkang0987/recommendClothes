917b07739b549fe1e33c33264ead03005684877c
minjeongkang <snowdrop1202@gmail.com>
2021-01-22 13:44:56 +0900

    refactor: API fetch 함수 try/catch문 추가

--- clothes/src/action/fetch.js
+++ clothes/src/action/fetch.js
@@ -1,6 +1,10 @@
 export const fetchData = {
   async getData({url})  {
-    const res = await fetch(`${url}`);
-    if (res.ok) return res.json();
+    try {
+      const res = await fetch(`${url}`);
+      return await res.json();
+    } catch(err) {
+      console.error(err);
+    }
   }
 }
