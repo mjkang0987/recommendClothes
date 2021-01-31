fdb8988d86293ddcf4ace94274cae6e1a78fa424
minjeongkang <snowdrop1202@gmail.com>
2021-01-31 23:49:57 +0900

    feat: CityContainer connect 함수 사용하여 store 연결

--- clothes/src/routes/cities/CitiesContainer.jsx
+++ clothes/src/routes/cities/CitiesContainer.jsx
@@ -1,9 +1,23 @@
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
 import CitiesPresenter from './CitiesPresenter';
 
-const CitiesContainer = _ => {
-  return (
-    <CitiesPresenter/>
-  )
-}
+const mapStateToProps = (state) => ({
+  current: state.current,
+  cities: state.cities
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onAdd: ({city}) => dispatch(actions.cityAdd),
+  onRemove: ({city}) => dispatch(actions.cityRemove),
+  onUpdate: ({current}) => {
+    dispatch(actions.cityUpdate({current}))
+  },
+});
+
+const CitiesContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CitiesPresenter);
 
 export default CitiesContainer;
