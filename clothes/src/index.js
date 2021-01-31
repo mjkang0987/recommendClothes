975db5fa23e14f71323ab9548fb2fd36a84ce5ad
minjeongkang <snowdrop1202@gmail.com>
2021-01-31 23:50:23 +0900

    feat: index.js Provider 추가

--- clothes/src/index.js
+++ clothes/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './styles/style.scss';
-import rootReducer from './store/modules';
+import reducer from './store/reducer'
 import App from './App';
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducer, devTools);
-console.log(store.getState());
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer, devTools);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
