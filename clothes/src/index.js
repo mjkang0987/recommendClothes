f87562c858929fd0f4c313bf8d389a9b1a5f1dce
minjeongkang <snowdrop1202@gmail.com>
2021-03-29 00:06:48 +0900

    refactor: 리듀서 폴더명 modules로 변경

--- clothes/src/index.js
+++ clothes/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducer/combineReducers';
+import rootReducer from './modules/combineReducers';
 import './styles/style.scss';
 import App from './App';
 
