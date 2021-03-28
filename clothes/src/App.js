9bd68bb6dd1c6f5070d0137c3921b7d3e9d77dd1
minjeongkang <snowdrop1202@gmail.com>
2021-03-29 00:06:10 +0900

    refactor: 상태관리 contextAPI에서 리덕스로 변경
    * Header 컴포넌트부터 변경작업으로 기존 컴포넌트 내용 삭제

--- clothes/src/App.js
+++ clothes/src/App.js
@@ -1,82 +1,39 @@
-import { createContext, useState } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import { useOnMounted } from './action/useOnMount';
-import { fetchData } from './utils/fetch';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { timeGet } from './actions/time';
 import Header from './components/Header';
-import Main from './routes/main/Main';
-import Cities from './routes/cities';
-import Footer from './components/Footer';
-import Loading from './components/Loading';
-import { API, INITIAL_WEATHER, INITIAL_TIME, TEMP } from './constants/constants';
 
-const {getData} = fetchData;
-const {URL, KEY} = API;
-const {ABSOLUTE} = TEMP;
-
-export const Weather = createContext({INITIAL_WEATHER, INITIAL_TIME});
-const App = _ => {
-  const [weather, setWeather] = useState(INITIAL_WEATHER);
-  const {LOCATION} = weather;
-
-  const getWeather = ({location = 'seoul'}) => {
-    return (async _ => {
-      const res = await getData({url: `${URL}?q=${location}${KEY}`});
-      const {main: temps, name: city, weather} = res;
-      const weatherData = {
-        TEMPS: {
-          NOW: Math.trunc(temps.temp - ABSOLUTE),
-          MIN: Math.trunc(temps.temp_min - ABSOLUTE),
-          MAX: Math.trunc(temps.temp_max - ABSOLUTE)
-        },
-        WEATHER: weather[0].description,
-        LOCATION: city.toLowerCase()
-      };
-      setWeather(weatherData);
-    })();
-  };
-
-  const initTime = new Date();
-  const timeObj = {
-    hour: initTime.getHours(),
-    minute: initTime.getMinutes()
-  };
-  const [time, setTime] = useState(timeObj);
-
-  const intervalTime = ({time}) => {
-    setInterval(_=> {
-      const currentTime = new Date();
-      const currentTimeObj = {
-        hour: currentTime.getHours(),
-        minute: currentTime.getMinutes()
-      }
-      setTime(currentTimeObj);
-    }, time);
-  };
-
-  useOnMounted(_ => {
-    (async _ => {
-      await getWeather({location: 'seoul'});
-    })();
-    intervalTime({time: 5000});
-  });
+const App = ({hour, minute, timeGet}) => {
+  setInterval(_ => {
+    const now = new Date();
+    const currentTime = {
+      hour: now.getHours(),
+      minute: now.getMinutes()
+    }
+    timeGet({now: currentTime});
+  }, 5000);
 
   return (
-    <Weather.Provider value={{weather, setWeather, getWeather, time}}>
-      <Router>
-        <Header/>
-        <Route
-          exact
-          path="/cities/:pn"
-          component={Cities}/>
-        <Route
-          path="/"
-          component={Main}/>
-        <Footer/>
-        {!LOCATION && <Loading/>}
-        <Redirect path="*" to="/" />
-      </Router>
-    </Weather.Provider>
+    <Router>
+      <Header
+        hour={hour}
+        minute={minute}/>
+    </Router>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  hour: state.timeReducer.hour,
+  minute: state.timeReducer.minute
+})
+
+const mapDispatchToProps = dispatch => ({
+  timeGet: ({now}) => {
+    dispatch(timeGet({now}));
+  }
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
