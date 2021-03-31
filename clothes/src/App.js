ec2c6ea6d64a2fb5cdd6e739e479f5c73b69cd2b
minjeongkang <snowdrop1202@gmail.com>
2021-03-31 23:20:10 +0900

    feat: openWeather API 돔 로드 이후 호출

--- clothes/src/App.js
+++ clothes/src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getTime } from './actions/time';
+import { useOnMounted } from './actions/useOnMount';
+import { fetchData } from './utils/fetch';
+import { API, TEMP } from './constants/constants';
 import Header from './components/Header';
 
+const {getData} = fetchData;
+const {URL, KEY} = API;
+const {ABSOLUTE} = TEMP;
 
-const App = ({time, getTime, getCity}) => {
-
+const App = ({time, getTime, weather, getCity}) => {
   const initTime = _ => {
     const initTime = new Date();
     const timeObj = {
@@ -16,13 +21,34 @@ const App = ({time, getTime, getCity}) => {
     };
     getTime({now: timeObj});
   }
+
+  const getWeather = ({location = 'seoul'}) => {
+    return (async _ => {
+      const res = await getData({url: `${URL}?q=${location}${KEY}`});
+      const {main: temps, name: city, weather} = res;
+      const weatherData = {
+        now: Math.trunc(temps.temp - ABSOLUTE),
+        min: Math.trunc(temps.temp_min - ABSOLUTE),
+        max: Math.trunc(temps.temp_max - ABSOLUTE),
+        desc: weather[0].description,
+        city: {
+          current: city.toLowerCase(),
+          // cities: [city.toLowerCase()]
+        }
+      }
+      getCity({weather: weatherData})
+    })();
+  };
   useOnMounted(_ => {
     initTime();
+    (async _ => {
+      await getWeather({location: 'seoul'});
+    })();
 
     setInterval(_ => {
       initTime();
     }, 3000);
-  }, [])
+  }, []);
   return (
     <Router>
       <Header time={time}/>
@@ -32,11 +58,15 @@ const App = ({time, getTime, getCity}) => {
 
 export default connect(
   state => ({
-    time: state.timeReducer.time
+    time: state.timeReducer.time,
+    weather: state.weatherReducer.weather
   }),
   dispatch => ({
     getTime: ({now}) => {
       dispatch(getTime({now}));
+    },
+    getCity: ({weather}) => {
+      dispatch(getCity({weather}));
     }
   })
 )(App);
